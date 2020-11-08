d96dd2be9b2c9dcc162b36866b34ae90ea27fb97
rahmatagungj <rahmatagungj@gmail.com>
2020-11-08 18:24:08 +0700

    Add new Police System for Rob

--- gangrob/server.lua
+++ gangrob/server.lua
@@ -1,6 +1,13 @@
 addEvent("robHouse",true)
 addEventHandler("robHouse",root, function (player,intid)
-	--local intid = 14
+	local theTeam = exports.pool:getElement("team", 1)
+	local theTeamName = getTeamName(theTeam)
+	local teamPlayers = getPlayersInTeam(theTeam)
+	if #teamPlayers == 1 then
+		print("TIDAK DAPAT MELAKUKAN ROB KARENA TIDAK ADA POLISI ONLINE")
+		return
+	end
+
 	if player and intid then
 		factionID = getElementData	(player,"faction")
 		if factionID then
@@ -23,18 +30,28 @@ addCommandHandler("rob",tes)
 addEvent("finishedRobbing",true)
 addEventHandler("finishedRobbing",root, function (player,intid)
 	if player and intid then
-		local success = math.random(1,2)
-		local police = math.random(1,3)
-		if success == 1 then -- izdodas atlauzt
-			outputChatBox("kamu masuk kedalam toko",player)
-			if police == 1 then
-				outputChatBox("Alarm peringatan berbunyi! kamu tidak punya banyak waktu sebelum polisi tiba",player)
-				triggerCops()
-			elseif police == 2 then
-				outputChatBox("Kamera CCTV sudah memantau, wajah kamu sudah terdeteksi!",player)
-			else
-				outputChatBox("Alarm belum berbunyi",player)
-			end
+		local success = math.random(1,1)
+		local police = math.random(1,2)
+		if success == 1 then 
+			-- giving money
+			if exports.global:giveMoney(source,uangRampok) then
+				local UANG_RAMPOK = exports.global:formatMoney(uangRampok)
+				outputChatBox("Selamat kamu mendapatkan uang senilai Rp. ".. THE_NEW .."",source,255,255,255)
+				--triggerClientEvent(source,"overlay:create:topright",source,"INFO","kamu mendapatkan hasil rampokan senilai Rp. ".. UANG_RAMPOK .."",6000,true,false,"info",false)
+			end 
+
+			setTimer(function()
+				if police == 1 then
+					outputChatBox("Alarm peringatan berbunyi! kamu tidak punya banyak waktu sebelum polisi tiba",player)
+					triggerCops()
+				else
+					outputChatBox("Alarm belum berbunyi",player)
+					setTimer(function()
+						outputChatBox("Alarm peringatan berbunyi! kamu tidak punya banyak waktu sebelum polisi tiba",player)
+						triggerCops()
+					end,5000,1,player)
+				end
+			end,5000,1,player)
 		end
 		if success == 2 then
 			outputChatBox("kamu gagal membuka pintu",player)
@@ -52,16 +69,17 @@ end)
 	
 
 function triggerCops()
-	local theTeam = getTeamFromName("Kepolisian Republic City")
+	--[[local theTeam = getTeamFromName("Kepolisian Republic City")
 		local teamPlayers = getPlayersInTeam(theTeam)
 		for key, value in ipairs(teamPlayers) do
 		local duty = tonumber(getElementData(value, "duty"))
 			if (duty>0) then
-			--	outputChatBox("[RADIO] Untuk semua unit, " .. areaName .. ".", value, 255, 194, 14)
-			--	outputChatBox("[RADIO] Vehicle was a ", value, 255, 194, 14)
-			--	outputChatBox("[RADIO] The plate is '"..  getVehiclePlateText ( theVehicle ) .."' and heading " .. direction .. ".", value, 255, 194, 14)
+			--outputChatBox("[RADIO] Untuk semua unit, " .. areaName .. ".", value, 255, 194, 14)
+			--outputChatBox("[RADIO] Vehicle was a ", value, 255, 194, 14)
+			--outputChatBox("[RADIO] The plate is '"..  getVehiclePlateText ( theVehicle ) .."' and heading " .. direction .. ".", value, 255, 194, 14)
 			end
-		end
+		end]]
+		print("polisi datang")
 end
 function convertServerTickToTimeStamp(tick) return getRealTime().timestamp+((getTickCount()*0.001)-(tick*0.001)); end
 	
@@ -79,4 +97,3 @@ function formatMilliseconds(milliseconds)
     hours = hours % 24 
     return string.format( "%02d:%02d", hours, minutes )   
 end 
-
